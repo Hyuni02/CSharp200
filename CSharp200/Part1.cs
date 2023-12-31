80648ca405811d410a48e0ce77a6d6d9f9bfdced
ErrorCode02 <jackjsh@naver.com>
2023-12-31 23:21:41 +0900

    009

--- CSharp200/Part1.cs
+++ CSharp200/Part1.cs
@@ -67,5 +67,18 @@ namespace CSharp200 {
                 Console.WriteLine("x < y");
             }
         }
+        public void func9() {
+            int sum1 = 0;
+            int sum2 = 0;
+            int one = 1;
+            int two = 2;
+            for(int i=0; i < 5; i++) {
+                sum1 += one;
+                sum2 += two;
+            }
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+        }
     }
 }
