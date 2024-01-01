d0b135b0d73e49acbdc809b869db4e9c2bcc048c
ErrorCode02 <jackjsh@naver.com>
2024-01-02 04:14:11 +0900

    017

--- CSharp200/Part2.cs
+++ CSharp200/Part2.cs
@@ -7,5 +7,12 @@
             Console.WriteLine(c1);
             //Console.WriteLine(c2);
         }
+        public void func17() {
+            string str = "string";
+            string str2 = "string2";
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+            Console.WriteLine(str + str2);
+        }
     }
 }
