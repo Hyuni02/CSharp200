aec5b31698de100c19ddda9c1d10767145d95650
ErrorCode02 <jackjsh@naver.com>
2024-01-03 17:12:36 +0900

    037

--- CSharp200/Part2.cs
+++ CSharp200/Part2.cs
@@ -143,5 +143,22 @@ namespace CSharp200 {
             }
             Console.WriteLine(count);
         }
+        public void func37() {
+            int a = 3;
+            switch (a) {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    break;
+                default:
+                    Console.WriteLine("no match");
+                    break;
+            }
+        }
     }
 }
