d31bfbacd65d45876f2761d5c722c74ac8b7f058
ErrorCode02 <jackjsh@naver.com>
2024-01-08 23:59:43 +0900

    054 055

--- CSharp200/Program.cs
+++ CSharp200/Program.cs
@@ -2,7 +2,7 @@
     internal class Program {
         static void Main(string[] args) {
             Part2 part = new Part2();
-            part.func53();
+            part.func55();
         }
     }
 }
